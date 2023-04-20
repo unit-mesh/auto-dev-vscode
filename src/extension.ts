bf265c7896eefa8207b78ccc148bb85cb8c845f3
CGQAQ <m.jason.liu@outlook.com>
2023-04-20 16:25:41 +0800

    +n deps

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,13 @@
 import * as vscode from 'vscode';
 
+const channel = vscode.window.createOutputChannel("AUTO-DEV-VSCODE")
 export function activate(context: vscode.ExtensionContext) {
+	// channel.show();
+	// channel.appendLine(JSON.stringify(vscode.languages.getLanguages()))
+	vscode.languages.getLanguages().then((v) => {
+		console.log("its", v)
+	}, (r) => console.log(r))
+
 	console.log('Congratulations, your extension "auto-dev-vscode" is now active!');
 
 	let disposable = vscode.commands.registerCommand('auto-dev-vscode.helloWorld', () => {
