9c6b7f3d6c713dc40768d74ef9e5868180733383
Wáng Wěi Tāo <softweitao@126.com>
2023-07-05 09:00:28 +0000

    chore: migrate from webpack to vite

---
+++ tsup.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'tsup'
+
+export default defineConfig({
+  entry: [
+    'src/extension.ts',
+  ],
+  format: ['cjs'],
+  shims: false,
+  dts: false,
+  external: [
+    'vscode',
+  ],
+})
