fbfc91e8d837da197687f4af720e160ce0ecc839
Phodal Huang <h@phodal.com>
2024-04-09 16:54:16 +0800

    refactor: update TreeSitterFile constructor #2
    - Update constructor parameters and formatting for better readability and maintainability.
    - Add support for IdentifierBlockRange in methodRanges() and getByQuery().refactor: improve TreeSitterFile class
    
    - Refactored constructor and methods in TreeSitterFile class for better readability and maintainability. Added IdentifierBlockRange for methodRanges().

--- src/document/TextRange.ts
+++ src/document/TextRange.ts
@@ -1,15 +1,25 @@
+import { SyntaxNode } from "web-tree-sitter";
+
 export class TextRange {
 	text: string;
 	start: { line: number; character: number };
 	end: { line: number; character: number };
 
 	constructor(
+		displayName: string = "",
 		start: { line: number; character: number },
-		end: { line: number; character: number },
-		displayName: string = ""
+		end: { line: number; character: number }
 	) {
 		this.start = start;
 		this.end = end;
 		this.text = displayName;
+	}
+
+	static fromNode(id: SyntaxNode) {
+		return new TextRange(
+			id.text,
+			{ line: id.startPosition.row, character: id.startPosition.column },
+			{ line: id.endPosition.row, character: id.endPosition.column },
+		);
 	}
 }
--- src/semantic-treesitter/TreeSitterFile.ts
+++ src/semantic-treesitter/TreeSitterFile.ts
@@ -2,109 +2,104 @@ import Parser, { Language, Tree } from "web-tree-sitter";
 import { TSLanguageConfig } from "./TSLanguageConfig";
 import { TSLanguage } from "./TreeSitterLanguage";
 import { TextRange } from "../document/TextRange";
+import { IdentifierBlockRange } from "../document/IdentifierBlockRange";
 
 export class TreeSitterFile {
-  private src: string;
-  private tree: Tree;
-  private langConfig: TSLanguageConfig;
-  private parser: Parser | undefined = undefined;
-  private language: Parser.Language;
+	private src: string;
+	private tree: Tree;
+	private langConfig: TSLanguageConfig;
+	private parser: Parser | undefined = undefined;
+	private language: Parser.Language;
 
-  constructor(
-    src: string,
-    tree: Tree,
-    tsLanguage: TSLanguageConfig,
-    parser: Parser,
-    language: Language
-  ) {
-    this.src = src;
-    this.tree = tree;
-    this.langConfig = tsLanguage;
-    this.parser = parser;
-    this.language = language;
-  }
+	constructor(
+		src: string,
+		tree: Tree,
+		tsLanguage: TSLanguageConfig,
+		parser: Parser,
+		language: Language
+	) {
+		this.src = src;
+		this.tree = tree;
+		this.langConfig = tsLanguage;
+		this.parser = parser;
+		this.language = language;
+	}
 
-  static async tryBuild(
-    src: string,
-    langId: string
-  ): Promise<TreeSitterFileError | TreeSitterFile> {
-    // no scope-res for files larger than 500kb
-    if (src.length > 500 * Math.pow(10, 3)) {
-      return TreeSitterFileError.FileTooLarge;
-    }
+	static async tryBuild(
+		src: string,
+		langId: string
+	): Promise<TreeSitterFileError | TreeSitterFile> {
+		// no scope-res for files larger than 500kb
+		if (src.length > 500 * Math.pow(10, 3)) {
+			return TreeSitterFileError.FileTooLarge;
+		}
 
-    const tsLang = TSLanguage.fromId(langId);
-    if (tsLang === undefined) {
-      return TreeSitterFileError.UnsupportedLanguage;
-    }
+		const tsLang = TSLanguage.fromId(langId);
+		if (tsLang === undefined) {
+			return TreeSitterFileError.UnsupportedLanguage;
+		}
 
-    const parser = new Parser();
-    let language: Language | undefined = undefined;
-    try {
-      language = await tsLang.grammar();
-      parser.setLanguage(language);
-    } catch (error) {
-      return TreeSitterFileError.LanguageMismatch;
-    }
+		const parser = new Parser();
+		let language: Language | undefined = undefined;
+		try {
+			language = await tsLang.grammar();
+			parser.setLanguage(language);
+		} catch (error) {
+			return TreeSitterFileError.LanguageMismatch;
+		}
 
-    if (!language) {
-      return TreeSitterFileError.LanguageMismatch;
-    }
+		if (!language) {
+			return TreeSitterFileError.LanguageMismatch;
+		}
 
-    // do not permit files that take >1s to parse
-    parser.setTimeoutMicros(Math.pow(10, 6));
+		// do not permit files that take >1s to parse
+		parser.setTimeoutMicros(Math.pow(10, 6));
 
-    const tree = parser.parse(src);
-    if (!tree) {
-      return TreeSitterFileError.ParseTimeout;
-    }
+		const tree = parser.parse(src);
+		if (!tree) {
+			return TreeSitterFileError.ParseTimeout;
+		}
 
-    return new TreeSitterFile(src, tree, tsLang, parser, language);
-  }
+		return new TreeSitterFile(src, tree, tsLang, parser, language);
+	}
 
-  methodRanges(): TextRange[] | TreeSitterFileError {
-    if (!this.parser) {
-      return TreeSitterFileError.QueryError;
-    }
+	methodRanges(): IdentifierBlockRange[] | TreeSitterFileError {
+		if (!this.parser) {
+			return TreeSitterFileError.QueryError;
+		}
 
-    return this.getByQuery(this.langConfig.methodQuery.scopeQuery);
-  }
+		return this.getByQuery(this.langConfig.methodQuery.scopeQuery);
+	}
 
-  private getByQuery(queryString: string): TextRange[] | TreeSitterFileError {
-    try {
-      const query = this.language.query(queryString);
-      const root = this.tree.rootNode;
-      const matches = query?.matches(root);
+	private getByQuery(queryString: string): IdentifierBlockRange[] | TreeSitterFileError {
+		try {
+			const query = this.language.query(queryString);
+			const root = this.tree.rootNode;
+			const matches = query?.matches(root);
 
-      return (
-        matches?.flatMap((match) => {
-          // name.definition.method
-          const node = match.captures[0].node;
-          // definition.method
-          const results = new TextRange(
-            {
-              line: node.startPosition.row,
-              character: node.startPosition.column,
-            },
-            {
-              line: node.endPosition.row,
-              character: node.endPosition.column,
-            },
-            match.captures[1].node.text ?? ""
-          );
-          return results;
-        }) ?? []
-      );
-    } catch (error) {
-      return TreeSitterFileError.QueryError;
-    }
-  }
+			return (
+				matches?.flatMap((match) => {
+					// name.definition.method
+					const identifierNode = match.captures[0].node;
+					const blockNode = match.captures[1].node;
+
+					// definition.method
+					return new IdentifierBlockRange(
+						TextRange.fromNode(identifierNode),
+						TextRange.fromNode(blockNode)
+					);
+				}) ?? []
+			);
+		} catch (error) {
+			return TreeSitterFileError.QueryError;
+		}
+	}
 }
 
 export enum TreeSitterFileError {
-  UnsupportedLanguage,
-  ParseTimeout,
-  LanguageMismatch,
-  QueryError,
-  FileTooLarge,
+	UnsupportedLanguage,
+	ParseTimeout,
+	LanguageMismatch,
+	QueryError,
+	FileTooLarge,
 }
