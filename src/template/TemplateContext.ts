3f31bb1fb899dd5160f3e7a292903ff9ad77e0d7
Phodal Huang <h@phodal.com>
2024-04-11 21:38:54 +0800

    feat(template): add Velocity-based template rendering system and related classes #2

---
+++ src/template/TemplateContext.ts
@@ -0,0 +1,7 @@
+export interface TemplateContext {
+}
+
+class EmptyContext implements TemplateContext {
+
+}
+
---
+++ src/template/TemplateRender.ts
@@ -0,0 +1,33 @@
+import Velocity from "velocityjs";
+import { TemplateContext } from "./TemplateContext.ts";
+import fs from "fs";
+
+export class TemplateRender {
+	private templateCache: { [filename: string]: string } = {};
+
+	/**
+	 * Retrieves the template content from the specified file.
+	 *
+	 * @param filename the name of the file containing the template
+	 * @return the content of the template as a string
+	 * @throws TemplateNotFoundError if the specified file cannot be found
+	 */
+	private retrieveDefaultTemplate(filename: string): string {
+		if (this.templateCache.hasOwnProperty(filename)) {
+			return this.templateCache[filename];
+		}
+
+		const string = fs.readFileSync(filename, "utf8");
+
+		this.templateCache[filename] = string;
+		return string;
+	}
+
+	private getDefaultFilePath(filename: string): string {
+		return filename;
+	}
+
+	public render(template: string, context: TemplateContext): string {
+		return Velocity.render(template, context);
+	}
+}
