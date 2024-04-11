186f7098e87ca59a82438c18c94e2352bb54d6c7
CGQAQ <m.jason.liu@outlook.com>
2024-04-11 14:48:47 +0800

    chore: add csp

--- src/webview/AutoDevWebviewViewProvider.ts
+++ src/webview/AutoDevWebviewViewProvider.ts
@@ -53,6 +53,7 @@ export class AutoDevWebviewViewProvider implements vscode.WebviewViewProvider {
   <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="Content-Security-Policy" content="img-src ${panel.webview.cspSource} https://images.unsplash.com; script-src ${panel.webview.cspSource}; style-src ${panel.webview.cspSource};">
     <title>Vite + React + TS</title>
     <script type="module" crossorigin src="${jsSrc}"></script>
     <link rel="stylesheet" crossorigin href="${cssSrc}">
