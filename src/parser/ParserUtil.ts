7f8a67184aa400e0237ebda1d77fb3bc2f69ea90
Phodal Huang <h@phodal.com>
2024-04-12 10:30:15 +0800

    refactor: extract to schema files

--- src/parser/ParserUtil.ts
+++ src/parser/ParserUtil.ts
@@ -1,10 +1,7 @@
 import vscode from "vscode";
 import Parser from "web-tree-sitter";
-import path from "path";
-import fs from "fs";
 
 import { getLanguageForFile } from "./TreeSitterParser";
-import { EXT_LANGUAGE_MAP } from "../language/ExtLanguageMap";
 
 export async function getParserForFile(uri: vscode.Uri, filepath: string) {
 	if (process.env.IS_BINARY) {
@@ -21,67 +18,6 @@ export async function getParserForFile(uri: vscode.Uri, filepath: string) {
 		return parser;
 	} catch (e) {
 		console.error("Unable to load language for file", filepath, e);
-		return undefined;
-	}
-}
-
-export function getQuerySource(filepath: string) {
-	const fullLangName = EXT_LANGUAGE_MAP[filepath.split(".").pop() ?? ""];
-	const sourcePath = path.join(__dirname, "semantic", `${fullLangName}.scm`);
-	if (!fs.existsSync(sourcePath)) {
-		throw new Error("cannot find file:" + sourcePath);
-	}
-
-	// TODO: use vscode.workspace.fs.readFile
-	return fs.readFileSync(sourcePath).toString();
-}
-
-export async function getSnippetsInFile(
-	uri: vscode.Uri,
-	filepath: string,
-	contents: string
-): Promise<(any & { title: string })[]> {
-	const lang = await getLanguageForFile(filepath, uri);
-	if (!lang) {
-		return [];
-	}
-	const parser = await getParserForFile(uri, filepath);
-	if (!parser) {
-		return [];
-	}
-	const ast = parser.parse(contents);
-	const query = lang?.query(getQuerySource(filepath));
-	const matches = query?.matches(ast.rootNode);
-
-	return (
-		matches?.flatMap((match) => {
-			const node = match.captures[0].node;
-			const title = match.captures[1].node.text;
-			const results = {
-				title,
-				content: node.text,
-				startLine: node.startPosition.row,
-				endLine: node.endPosition.row,
-			};
-			return results;
-		}) ?? []
-	);
-}
-
-export async function getAst(
-	uri: vscode.Uri,
-	filepath: string,
-	fileContents: string
-): Promise<Parser.Tree | undefined> {
-	const parser = await getParserForFile(uri, filepath);
-
-	if (!parser) {
-		return undefined;
-	}
-
-	try {
-		return parser.parse(fileContents);
-	} catch (e) {
 		return undefined;
 	}
 }
