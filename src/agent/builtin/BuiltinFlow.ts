3d5e505fce35d99bfcfb06033693b5d8dc56c9f2
Phodal Huang <h@phodal.com>
2024-04-13 15:04:38 +0800

    feat(agent): introduce CustomAgentConfig and related classes for custom agent configuration #9
    This commit introduces a new interface `TaskFlow` and its related classes for custom agent configuration. It includes methods for clarifying user requirements, designing task flows based on given context, executing the designed flows, and fixing errors in the flow.

---
+++ src/agent/builtin/BuiltinFlow.ts
@@ -0,0 +1,43 @@
+/**
+ * AutoDev TaskFlow is the built-in Workflow for AutoDev, see in [#81](https://github.com/unit-mesh/auto-dev/issues/81)
+ */
+interface TaskFlow<Tasking> {
+	/**
+	 * This method is used to clarify the purpose of user's requirement.
+	 * It returns a string will specify format, will parse in design()
+	 * For example, tableNames in SQL:
+	 * ```markdown
+	 * [item1, item2, item3]
+	 * ```
+	 * @return A string representing the documentation for the class.
+	 */
+	clarify(): string;
+
+	/**
+	 * This method is used to ask LLM to design the tasking flow based on the given context.
+	 * It returns a string will specify format, will parse in execute()
+	 *
+	 * @param context The context for designing the task flow.
+	 */
+	design(context: any): Tasking[];
+
+	/**
+	 * Executes the last step of the task flow.
+	 *
+	 * Like:
+	 * - Execute SQL in [cc.unitmesh.database.flow.GenSqlFlow]
+	 * - Execute HTTP request in HttpRequest
+	 * - Execute shell command in ShellCommand
+	 *
+	 * @return A string representing the result of the execution.
+	 */
+	execute(context: any): string;
+
+	/**
+	 * This method is used to fix the errors in the task flow.
+	 *
+	 * @param errors The errors that need to be fixed.
+	 * @return A string representing the fixed errors.
+	 */
+	fix(errors: string): string;
+}
