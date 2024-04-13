d0aab1341a159b60b0fc370e13b3381f8624ad2c
Phodal Huang <h@phodal.com>
2024-04-13 14:39:50 +0800

    feat(semantic): add TestGenProvider interface and TestGenContext interface for test generation #9

---
+++ src/semantic/_base/TestGenProvider.ts
@@ -0,0 +1,14 @@
+import { CodeFile, CodeStructure } from "../../codemodel/CodeFile";
+
+export interface TestGenContext {
+	isNewFile?: boolean,
+	relatedClasses: CodeStructure[],
+	testClassName: string,
+	language: string,
+	currentObject: CodeStructure,
+}
+
+export interface TestGenProvider {
+	findOrCreateTestFile(sourceFile: CodeFile, element: any): Promise<TestGenContext>;
+	lookupRelevantClass(element: any): Promise<CodeStructure>;
+}
