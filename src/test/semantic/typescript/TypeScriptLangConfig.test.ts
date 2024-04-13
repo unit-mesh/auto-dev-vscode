4a474f33b94d9b3e4b5a888382b9d2b2e47e7213
Phodal Huang <h@phodal.com>
2024-04-13 21:37:25 +0800

    feat(typescript): refactor method detection in TypeScript files #9

--- src/test/semantic/typescript/TypeScriptLangConfig.test.ts
+++ src/test/semantic/typescript/TypeScriptLangConfig.test.ts
@@ -1,10 +1,22 @@
 import { SyntaxNode } from "web-tree-sitter";
-
-const Parser = require("web-tree-sitter");
 import { TestLanguageService } from "../../TestLanguageService";
 import { TypeScriptLangConfig } from "../../../semantic/typescript/TypeScriptLangConfig";
 
+const Parser = require("web-tree-sitter");
+
 describe('TypeScriptLangConfig', () => {
+	let parser: any;
+	let grammar: any;
+
+	beforeEach(async () => {
+		await Parser.init();
+		parser = new Parser();
+		const languageService = new TestLanguageService(parser);
+
+		grammar = await TypeScriptLangConfig.grammar(languageService, "typescript")!!;
+		parser.setLanguage(grammar);
+	});
+
 	it('should identify methods of TypeScript', async () => {
 		const sampleCode = `function hello() {
   console.log("hello, world");
@@ -23,14 +35,6 @@ class Greeter {
 }
 `;
 
-		await Parser.init();
-		const parser = new Parser();
-		const languageService = new TestLanguageService(parser);
-
-		let grammar = await TypeScriptLangConfig.grammar(languageService, "typescript")!!;
-		parser.setLanguage(grammar);
-
-
 		const query = grammar!!.query(TypeScriptLangConfig.methodQuery.scopeQuery);
 		const root = parser.parse(sampleCode).rootNode;
 		const matches = query!!.matches(root);
@@ -38,9 +42,14 @@ class Greeter {
 		expect(matches).toBeDefined();
 		expect(matches.length).toBe(3);
 
-		const nodes: SyntaxNode[] = matches.map((match: any) => match.captures[0].node);
-		expect(nodes[0].text).toBe("function hello() {\n  console.log(\"hello, world\");\n}");
-		expect(nodes[1].text).toBe("constructor(message: string) {\n    this.greeting = message;\n  }");
-		expect(nodes[2].text).toBe("greet() {\n    return \"Hello, \" + this.greeting;\n  }");
+		const bodyNodes: SyntaxNode[] = matches.map((match: any) => match.captures[0].node);
+		expect(bodyNodes[0].text).toBe("function hello() {\n  console.log(\"hello, world\");\n}");
+		expect(bodyNodes[1].text).toBe("constructor(message: string) {\n    this.greeting = message;\n  }");
+		expect(bodyNodes[2].text).toBe("greet() {\n    return \"Hello, \" + this.greeting;\n  }");
+
+		const methodNameNodes: SyntaxNode[] = matches.map((match: any) => match.captures[1].node);
+		expect(methodNameNodes[0].text).toBe("hello");
+		expect(methodNameNodes[1].text).toBe("constructor");
+		expect(methodNameNodes[2].text).toBe("greet");
 	});
 });
