ee76c56e7f8f208345dd59e85259fb6a85bd02cc
Phodal Huang <h@phodal.com>
2024-04-15 20:30:38 +0800

    feat(java): add JavaTestGenProvider and related initialization logic.

--- src/semantic/TestGenProviderManager.ts
+++ src/semantic/TestGenProviderManager.ts
@@ -1,14 +1,16 @@
 import { TestGenProvider } from "./_base/TestGenProvider";
 import { SupportedLanguage } from "../language/SupportedLanguage";
+import { DefaultLanguageService } from "../language/service/DefaultLanguageService";
+import { JavaTestGenProvider } from "./java/JavaTestGenProvider";
 
 export class TestGenProviderManager {
 	private testGenMap: Map<SupportedLanguage, TestGenProvider> = new Map();
 
 	async init() {
 		let map: Map<string, TestGenProvider> = new Map();
-		// const testGen = new JavaTestGenProvider();
-		// await testGen.init(new DefaultLanguageService());
-		// map.set("java", testGen);
+		const testGen = new JavaTestGenProvider();
+		await testGen.init(new DefaultLanguageService());
+		map.set("java", testGen);
 		this.testGenMap = map;
 	}
 
---
+++ src/semantic/java/JavaTestGenProvider.ts
@@ -0,0 +1,24 @@
+import { TestGenContext, TestGenProvider } from "../_base/TestGenProvider";
+import { CodeFile, CodeStructure } from "../../codemodel/CodeFile";
+import { TSLanguageService } from "../../language/service/TSLanguageService";
+
+export class JavaTestGenProvider implements TestGenProvider {
+	private context: TestGenContext | undefined;
+	private languageService: TSLanguageService | undefined;
+
+	constructor(context?: TestGenContext) {
+		this.context = context;
+	}
+
+	async init(defaultLanguageService: TSLanguageService) {
+		this.languageService = defaultLanguageService;
+	}
+
+	findOrCreateTestFile(sourceFile: CodeFile, element: any): Promise<TestGenContext> {
+		return Promise.resolve(this.context!!);
+	}
+
+	lookupRelevantClass(element: any): Promise<CodeStructure> {
+		throw new Error("Method not implemented.");
+	}
+}
