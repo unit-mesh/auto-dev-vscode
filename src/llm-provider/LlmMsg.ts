80f98ad9b838b332926e0f43e6477dcdbf82c42f
Phodal Huang <h@phodal.com>
2024-04-18 19:49:19 +0800

    refactor: remove unused classes

--- src/llm-provider/LlmMsg.ts
+++ src/llm-provider/LlmMsg.ts
@@ -1,24 +1,7 @@
-export class LlmMsg {
-	static fromMap(msgs: { [key: string]: string }): ChatMessage[] {
-		return Object.entries(msgs).map(([key, value]) => ({
-			role: toChatRole(key),
-			content: value,
-			name: null
-		}));
-	}
-}
-
 export interface ChatMessage {
 	role: ChatRole;
 	content: string;
 	name?: string | null;
-}
-
-export enum FinishReason {
-	Stopped = "stop",
-	ContentFiltered = "content_filter",
-	FunctionCall = "function_call",
-	TokenLimitReached = "length"
 }
 
 export enum ChatRole {
