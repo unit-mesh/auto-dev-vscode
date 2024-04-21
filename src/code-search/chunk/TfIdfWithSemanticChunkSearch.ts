01c4067cd3b588ddc4885e849a14a2928fc8adc4
Phodal Huang <h@phodal.com>
2024-04-21 22:13:59 +0800

    feat(search): add cancellation token support to TfIdfWithSemanticChunkSearch #15

--- src/code-search/chunk/TfIdfWithSemanticChunkSearch.ts
+++ src/code-search/chunk/TfIdfWithSemanticChunkSearch.ts
@@ -1,6 +1,7 @@
 import natural, { TfIdf } from "natural";
 
 import { SemanticSearch } from "../chunk-strategy/ChunkSearchStrategy";
+import { CancellationToken } from "vscode";
 
 export class TfIdfWithSemanticChunkSearch extends SemanticSearch {
 	private tfidf: TfIdf;
@@ -17,4 +18,14 @@ export class TfIdfWithSemanticChunkSearch extends SemanticSearch {
 	async toSemanticChunks(similarFiles: string[], currentFile: string) {
 		throw new Error("Method not implemented.");
 	}
+}
+
+function withCancellation<T>(promise: Promise<T>, cancellationToken: CancellationToken, result: T): Promise<T> {
+	return new Promise<T>((resolve, reject) => {
+		const cancellationListener = cancellationToken.onCancellationRequested(() => {
+			cancellationListener.dispose();
+			resolve(result);
+		});
+		promise.then(resolve, reject).finally(() => cancellationListener.dispose());
+	});
 }
