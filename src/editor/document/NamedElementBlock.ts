92b1165ac848579260a65873bdc350379bc7eda8
Phodal Huang <h@phodal.com>
2024-04-23 15:45:21 +0800

    feat(editor): add documentation for NamedElementBlock class #15
    Added detailed documentation for the NamedElementBlock class in the file `NamedElementBlock.ts`, explaining its purpose and structure with a Java example.

--- src/editor/document/NamedElementBlock.ts
+++ src/editor/document/NamedElementBlock.ts
@@ -1,12 +1,32 @@
 import { BlockRange } from "./BlockRange";
 import { CodeElementType } from "../codemodel/CodeElementType";
 
+/**
+ * Represents a named element block for AST element.
+ * For example, in Java, a named element block could be a class or a method.
+ *
+ *```java
+ * class HelloWorld {
+ *   // This is a method comment
+ * 	 public static void main(string[] args) {
+ * 	 		System.Out.Println("Hello " + args[0]);
+ * 	 	}
+ * }
+ *```
+ *
+ * In the above example, if we present the `main` method as a named element block
+ * - blockRange: `public static void main(string[] args) { ... }`
+ * - identifierRange: `main`
+ * - codeElementType: `CodeElementType.Method`
+ * - blockContent: `public static void main(string[] args) { ... }`
+ * - commentRange: `// This is a method comment`
+ */
 export class NamedElementBlock {
-	commentRange: BlockRange | undefined;
 	blockRange: BlockRange;
 	identifierRange: BlockRange;
 	codeElementType: CodeElementType;
 	blockContent: string;
+	commentRange: BlockRange | undefined;
 
 	constructor(
 		blockRange: BlockRange,
