37abaa7d7226d35ce81345fbc306662d61dfbd32
Phodal Huang <h@phodal.com>
2024-04-24 21:19:17 +0800

    refactor(editor): remove unused CodeStructure import
    Removed unused import of CodeStructure in CommentUmlPresenter.ts file.

--- src/editor/codemodel/presenter/CommentUmlPresenter.ts
+++ src/editor/codemodel/presenter/CommentUmlPresenter.ts
@@ -1,5 +1,5 @@
 import { PlantUMLPresenter } from "./PlantUMLPresenter";
-import { CodeFile, CodeStructure } from "../CodeFile";
+import { CodeFile } from "../CodeFile";
 import { LANGUAGE_LINE_COMMENT_MAP } from "../../language/LanguageCommentMap";
 
 export class CommentUmlPresenter extends PlantUMLPresenter {
