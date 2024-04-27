38f3a6a7a5080747ac3ff14536f41801d0b20be9
Phodal Huang <h@phodal.com>
2024-04-27 10:52:49 +0800

    feat(embedding): replace LocalModelEmbedding with TransformersEmbeddingProvider #19
    The file LocalModelEmbedding.ts has been deleted and replaced with TransformersEmbeddingProvider.ts. This new file includes the implementation of a new embedding provider that uses the Transformers library for feature extraction. The new provider also includes a mechanism for grouping chunks for more efficient processing.

--- src/code-search/embedding/LocalModelEmbedding.ts
+++
@@ -1,5 +0,0 @@
-export class LocalModelEmbedding {
-	public static async embed(text: string): Promise<number[]> {
-		return text.split('').map((char) => char.charCodeAt(0));
-	}
-}
---
+++ src/code-search/embedding/TransformersEmbeddingProvider.ts
@@ -0,0 +1,60 @@
+import { Embedding } from "./Embedding";
+import { EmbeddingProvider } from "./EmbeddingProvider";
+const { env, pipeline } = require("@xenova/transformers");
+
+env.allowLocalModels = true;
+env.allowRemoteModels = false;
+// if (typeof window === "undefined") {
+// 	// The embeddings provider should just never be called in the browser
+// 	env.localModelPath = path.join(__dirname, "..", "models");
+// }
+
+class EmbeddingsPipeline {
+	static task = "feature-extraction";
+	static model = "all-MiniLM-L6-v2";
+	static instance: any | null = null;
+
+	static async getInstance() {
+		if (this.instance === null) {
+			this.instance = await pipeline(this.task, this.model);
+		}
+
+		return this.instance;
+	}
+}
+
+export class TransformersEmbeddingProvider implements EmbeddingProvider {
+	id = "transformersJs";
+
+	static MaxGroupSize: number = 4;
+
+	async embed(chunks: string[]): Promise<Embedding[]> {
+		let extractor = await EmbeddingsPipeline.getInstance();
+
+		if (!extractor) {
+			throw new Error("TransformerJS embeddings pipeline is not initialized");
+		}
+
+		if (chunks.length === 0) {
+			return [];
+		}
+
+		let outputs = [];
+		for (
+			let i = 0;
+			i < chunks.length;
+			i += TransformersEmbeddingProvider.MaxGroupSize
+		) {
+			let chunkGroup = chunks.slice(
+				i,
+				i + TransformersEmbeddingProvider.MaxGroupSize,
+			);
+			let output = await extractor(chunkGroup, {
+				pooling: "mean",
+				normalize: true,
+			});
+			outputs.push(...output.tolist());
+		}
+		return outputs;
+	}
+}
