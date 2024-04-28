32be861d5c5d33960150f86e1b5b8b6614d6499c
Phodal Huang <h@phodal.com>
2024-04-28 17:39:58 +0800

    test(codesearch): update test case in CodeChunker.test.ts #19
    Updated the test case in CodeChunker.test.ts to use more complex sample code. The new code includes a Node class and a Graph class, replacing the previous simple HelloWorld class. The expected results have been adjusted accordingly.

--- src/test/codesearch/CodeChunker.test.ts
+++ src/test/codesearch/CodeChunker.test.ts
@@ -1,6 +1,7 @@
-import { ChunkWithoutID } from "../../code-search/chunk/Chunk";
-
+// eslint-disable-next-line @typescript-eslint/naming-convention
 const Parser = require("web-tree-sitter");
+
+import { ChunkWithoutID } from "../../code-search/chunk/Chunk";
 import { parsedCodeChunker } from "../../code-search/chunk/util/CodeChunkerUtil";
 import { JavaLangConfig } from "../../code-context/java/JavaLangConfig";
 import { TestLanguageService } from "../TestLanguageService";
@@ -18,14 +19,58 @@ describe('CodeChunk for Java', () => {
 	});
 
 	it('should success', async () => {
-		let codeChunker: AsyncGenerator<ChunkWithoutID> = parsedCodeChunker(parser, "public class HelloWorld { }", 100);
+		let nodeCode = `class Node {
+    int value;
+    List<Node> neighbors;
+
+    public Node(int value) {
+        this.value = value;
+        this.neighbors = new ArrayList<>();
+    }
+
+    public void addNeighbor(Node neighbor) {
+        neighbors.add(neighbor);
+    }
+}`;
+
+		let graphCode = `class Graph {
+    List<Node> nodes;
+
+    public Graph() {
+        this.nodes = new ArrayList<>();
+    }
+
+    // 添加节点
+    public void addNode(Node node) {
+        nodes.add(node);
+    }
+
+    // 添加边
+    public void addEdge(Node from, Node to) {
+        from.addNeighbor(to);
+        to.addNeighbor(from); // 如果是无向图，需要双向连接
+    }
+}`
+
+		let sampleCode = `
+import java.util.*;
+
+// 节点类
+${nodeCode}
+
+// 图类
+${graphCode}
+`;
+		let codeChunker: AsyncGenerator<ChunkWithoutID> = parsedCodeChunker(parser, sampleCode, 100);
 		const results = [];
 
 		for await (let chunk of codeChunker) {
 			results.push(chunk);
 		}
 
-		expect(results.length).toBe(1);
-		expect(results[0].content).toBe("public class HelloWorld { }");
+		expect(results.length).toBe(2);
+
+		expect(results[0].content).toBe(nodeCode);
+		expect(results[1].content).toBe(graphCode);
 	});
 });
