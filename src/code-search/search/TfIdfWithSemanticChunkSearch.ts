b49dcfd7ec9e305ec78ad23ad45b238f6fe7781b
Phodal Huang <h@phodal.com>
2024-04-28 23:10:52 +0800

    feat(code-search): add term splitting functionality and tests #19
    Added a new method `splitTerms` in `TfIdfWithSemanticChunkSearch` class to split input terms based on certain patterns. This method will help in better search functionality by considering individual parts of compound terms. Also added corresponding tests to verify the correct functionality of term splitting.

--- src/code-search/search/TfIdfWithSemanticChunkSearch.ts
+++ src/code-search/search/TfIdfWithSemanticChunkSearch.ts
@@ -1,5 +1,6 @@
 import natural, { TfIdf } from "natural";
 import { TfIdfCallback } from "natural/lib/natural/tfidf";
+import { Uri } from "vscode";
 
 /**
  * // todo: spike for customize tfidf from: https://github.com/NaturalNode/natural/blob/master/lib/natural/tfidf/tfidf.js
@@ -14,7 +15,7 @@ import { TfIdfCallback } from "natural/lib/natural/tfidf";
  * The `search` method is used to calculate the TfIdf values for a given query. It accepts a string as a query and an optional callback function.
  * The method returns the TfIdf values for the query.
  *
- * For more information about Natural's TfIdf, please refer to the following link: https://naturalnode.github.io/natural/tfidf.html
+ * For validIdentifierPattern information about Natural's TfIdf, please refer to the following link: https://naturalnode.github.io/natural/tfidf.html
  *
  * Example usage of the class is provided in the class description.
  */
@@ -33,6 +34,45 @@ export class TfIdfWithSemanticChunkSearch {
 
 	search(query: string, callback?: TfIdfCallback) {
 		return this.tfidf.tfidfs(query, callback);
+	}
+
+	private static identifierPattern = /(?<![\p{Alphabetic}\p{Number}_$])[\p{Letter}_$][\p{Alphabetic}\p{Number}_$]{2,}(?![\p{Alphabetic}\p{Number}_$])/gu;
+	private static camelCaseRegex = /(?<=[a-z$])(?=[A-Z])/g;
+	private static numericRegex = /^(\D+)\p{Number}+$/u;
+	private static validIdentifierPattern = /[\p{Alphabetic}_$]{3,}/gu;
+
+	static* splitTerms(input: string) {
+		const toLowerCase = (term: string) => term.toLowerCase();
+
+		let namingMatch = input.matchAll(this.identifierPattern);
+		for (let [term] of namingMatch) {
+			let uniqueTerms = new Set();
+			uniqueTerms.add(toLowerCase(term));
+
+			let splitTerms = [];
+			let camelCaseSplit = term.split(this.camelCaseRegex);
+			if (camelCaseSplit.length > 1) {
+				splitTerms.push(...camelCaseSplit);
+			}
+
+			let underscoreSplit = term.split('_');
+			if (underscoreSplit.length > 1) {
+				splitTerms.push(...underscoreSplit);
+			}
+
+			let numberSuffixMatch = term.match(this.numericRegex);
+			if (numberSuffixMatch) {
+				splitTerms.push(numberSuffixMatch[1]);
+			}
+
+			for (let splitTerm of splitTerms) {
+				if (splitTerm.length > 2 && this.validIdentifierPattern.test(splitTerm)) {
+					uniqueTerms.add(toLowerCase(splitTerm));
+				}
+			}
+
+			yield* uniqueTerms;
+		}
 	}
 }
 
