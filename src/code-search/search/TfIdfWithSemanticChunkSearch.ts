231c51adfebad66e55aaddbdc9263998efbc9a6c
Phodal Huang <h@phodal.com>
2024-04-29 09:42:56 +0800

    refactor(code-search): move term splitting logic to new TermSplitter class #19
    The term splitting logic, which was previously part of the TfIdfWithSemanticChunkSearch class, has been moved to a new TermSplitter class. This change improves code organization and separation of concerns. The corresponding tests have also been updated to use the new TermSplitter class.

--- src/code-search/search/TfIdfWithSemanticChunkSearch.ts
+++ src/code-search/search/TfIdfWithSemanticChunkSearch.ts
@@ -34,61 +34,6 @@ export class TfIdfWithSemanticChunkSearch {
 	search(query: string, callback?: TfIdfCallback) {
 		return this.tfidf.tfidfs(query, callback);
 	}
-
-	private static allPattern = /(?<![\p{Alphabetic}\p{Number}_$])[\p{Letter}_$][\p{Alphabetic}\p{Number}_$]{2,}(?![\p{Alphabetic}\p{Number}_$])/gu;
-	private static camelCasePattern = /(?<=[a-z$])(?=[A-Z])/g;
-	private static numericPattern = /^([\D]+)\p{Number}+$/u;
-
-	/**
-	 * The `splitTerms` method is a static generator function that splits the input string into terms based on the naming styl
-	 * e of the identifiers. It supports three naming styles: CamelCase, Numeric, and underscore_case.
-	 *
-	 * @param input - The input string to be split into terms.
-	 *
-	 * The method works as follows:
-	 * 1. It matches all patterns in the input string.
-	 * 2. For each match, it creates a new set to store unique terms, and adds the lowercase version of the match to this set.
-	 * 3. It then splits the match into terms based on the CamelCase, underscore_case, and Numeric naming styles, and adds these terms to the terms array.
-	 * 4. If the term is longer than 2 characters and matches the alphabetic pattern, it is added to the set of unique terms.
-	 * 5. Finally, it yields each unique term.
-	 *
-	 * Note: The method uses the `matchAll`, `split`, and `match` methods of the String object, and the `add`
-	 * method of the Set object. It also uses the spread operator (...) to add multiple elements to an array.
-	 *
-	 * @yield {String} - Yields each unique term.
-	 *
-	 * todo: add support for chinese characters??
-	 */
-	static* splitTerms(input: string) {
-		let matchAll = input.matchAll(this.allPattern);
-		for (let [match_] of matchAll) {
-			let uniqueTerms: Set<String> = new Set();
-			uniqueTerms.add(match_.toLowerCase());
-
-			let terms = [];
-
-			let camelCaseSplits = match_.split(this.camelCasePattern);
-			if(camelCaseSplits.length > 1) {
-				terms.push(...camelCaseSplits);
-			}
-
-			let underscoreSplit = match_.split('_');
-			if (underscoreSplit.length > 1) {
-				terms.push(...underscoreSplit);
-			}
-
-			let numberSuffixMatch = match_.match(this.numericPattern);
-			numberSuffixMatch && terms.push(numberSuffixMatch[1]);
-
-			for (let term of terms) {
-				if (term.length > 2 && /[\p{Alphabetic}_$]{3,}/gu.test(term)) {
-					uniqueTerms.add(term.toLowerCase());
-				}
-			}
-
-			yield* uniqueTerms;
-		}
-	}
 }
 
 
--- src/test/codesearch/SplitTerms.test.ts
+++ src/test/codesearch/SplitTerms.test.ts
@@ -1,13 +1,13 @@
-import { TfIdfWithSemanticChunkSearch } from "../../code-search/search/TfIdfWithSemanticChunkSearch";
+import { TermSplitter } from "../../code-search/search/tfidf/TermSplitter";
 
-describe('TfIdfWithSemanticChunkSearch', () => {
+describe('TermSplitter', () => {
 	describe('splitTerms', () => {
 		it('should split terms correctly', () => {
 			const input = 'HelloWorld_helloWorld123';
 			const expectedOutput: string[] = ['helloworld_helloworld123', 'hello', 'world_hello', 'world123', 'helloworld',
 				'helloworld123', 'helloworld_helloworld'];
 
-			const generator = TfIdfWithSemanticChunkSearch.splitTerms(input);
+			const generator = TermSplitter.splitTerms(input);
 			const output = Array.from(generator);
 			expect(output).to.deep.equal(expectedOutput);
 		});
@@ -15,7 +15,7 @@ describe('TfIdfWithSemanticChunkSearch', () => {
 		it('should handle empty string', () => {
 			const input = '';
 			const expectedOutput: string[] = [];
-			const generator = TfIdfWithSemanticChunkSearch.splitTerms(input);
+			const generator = TermSplitter.splitTerms(input);
 			const output = Array.from(generator);
 			expect(output).to.deep.equal(expectedOutput);
 		});
@@ -23,7 +23,7 @@ describe('TfIdfWithSemanticChunkSearch', () => {
 		it('should handle string with no terms to split', () => {
 			const input = 'hello';
 			const expectedOutput: string[] = ['hello'];
-			const generator = TfIdfWithSemanticChunkSearch.splitTerms(input);
+			const generator = TermSplitter.splitTerms(input);
 			const output = Array.from(generator);
 			expect(output).to.deep.equal(expectedOutput);
 		});
@@ -31,7 +31,7 @@ describe('TfIdfWithSemanticChunkSearch', () => {
 		it('should handle string with special characters', () => {
 			const input = 'hello_world$123';
 			const expectedOutput: string[] = ['hello_world$123', 'hello', 'world$123', 'hello_world$'];
-			const generator = TfIdfWithSemanticChunkSearch.splitTerms(input);
+			const generator = TermSplitter.splitTerms(input);
 			const output = Array.from(generator);
 			expect(output).to.deep.equal(expectedOutput);
 		});
