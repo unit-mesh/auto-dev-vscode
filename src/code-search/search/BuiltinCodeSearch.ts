f12e2bcc48f4f4ba6245a53994573d26bd453cbe
Phodal Huang <h@phodal.com>
2024-04-29 17:03:31 +0800

    feat(ci): add coverage report generation and rename BuiltinCodeSearch #19
    This commit adds a new step in the CI workflow to generate a coverage report using the `vitest-coverage-report-action`. This step will always run, even if the tests are failing.
    
    Additionally, the `BuiltinCodeSearch` class has been renamed to `BuiltinFulltextCodeSearch` to better reflect its functionality. This class is designed to perform full-text code searches in a given set of items.

--- src/code-search/search/BuiltinCodeSearch.ts
+++
@@ -1,9 +0,0 @@
-import { CancellationToken } from "vscode";
-import { SemanticSearch } from "./_base/SemanticSearch";
-import { SearchOptions } from "./_base/SearchOptions";
-
-export class BuiltinCodeSearch implements SemanticSearch {
-	searchChunks(items: string[], maxResults: number, options: SearchOptions, cancellationToken: CancellationToken): Promise<any[]> {
-		throw new Error("Method not implemented.");
-	}
-}
