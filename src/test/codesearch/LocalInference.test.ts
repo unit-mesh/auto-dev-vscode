f06554e9d9e261dede1fd0f5e7538b5212f06192
Phodal Huang <h@phodal.com>
2024-04-30 21:34:44 +0800

    fix(test): remove LocalEmbeddingProvider from LocalInference test
    The LocalEmbeddingProvider import and its usage have been removed from the LocalInference test in the src/test/codesearch/LocalInference.test.ts file. This change is

--- src/test/codesearch/LocalInference.test.ts
+++ src/test/codesearch/LocalInference.test.ts
@@ -1,10 +1,5 @@
-import { LocalEmbeddingProvider } from "../../code-search/embedding/LocalEmbeddingProvider";
-
 describe('LocalInference', () => {
 	it.skip('should encoding', async () => {
-		let localInference = new LocalEmbeddingProvider();
-		await localInference.init();
-		let promise = await localInference.embed("test");
-		console.log(promise);
+		//
 	});
 });
