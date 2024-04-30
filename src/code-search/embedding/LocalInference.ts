a5a450bd6cdf44cffc9e9cf5faa2a4f07bdb9c1c
Phodal Huang <h@phodal.com>
2024-04-30 17:29:50 +0800

    refactor(embedding): separate initialization from embedding in LocalInference #19
    The LocalInference class has been refactored to separate the initialization of the environment, tokenizer, and session from the embedding process. This change improves the structure of the code and makes it more readable and maintainable. The `init` method now handles the initialization, while the `embed` method focuses on the embedding process.

--- src/code-search/embedding/LocalInference.ts
+++ src/code-search/embedding/LocalInference.ts
@@ -6,28 +6,34 @@ import { mean_pooling, reshape } from "./meanPooling";
 const ort = require('onnxruntime-node');
 
 export class LocalInference {
-	async embed(sequence: string): Promise<any> {
-		const { env, AutoTokenizer, Tensor } = await import('@xenova/transformers');
+	env: any;
+	tokenizer: any;
+	session: InferenceSession | undefined;
 
+	async init() {
+		const { env, AutoTokenizer, Tensor } = await import('@xenova/transformers');
+		this.env = env;
 		env.allowLocalModels = true;
 		let modelPath = path.join(__dirname, "models", "all-MiniLM-L6-v2", "onnx", "model_quantized.onnx");
 
-		// try {
-		let tokenizer = await AutoTokenizer.from_pretrained("all-MiniLM-L6-v2", {
+		this.tokenizer = await AutoTokenizer.from_pretrained("all-MiniLM-L6-v2", {
 			local_files_only: true,
 		});
 
-		let encodings = tokenizer(sequence);
-
-		let session: InferenceSession = await ort.InferenceSession.create(modelPath, {
+		this.session = await ort.InferenceSession.create(modelPath, {
 			executionProviders: ['cpu']
 		});
+	}
+
+	async embed(sequence: string): Promise<any> {
+		const { Tensor } = await import('@xenova/transformers');
+		let encodings = this.tokenizer(sequence);
 
 		const inputIdsTensor = new ONNXTensor('int64', new BigInt64Array(encodings.input_ids.cpuData), encodings.input_ids.dims);
 		const attentionMaskTensor = new ONNXTensor('int64', new BigInt64Array(encodings.attention_mask.cpuData), encodings.attention_mask.dims);
 		const tokenTypeIdsTensor = new ONNXTensor('int64', new BigInt64Array(encodings.token_type_ids.cpuData), encodings.token_type_ids.dims);
 
-		const outputs: InferenceSession.ReturnType = await session.run({
+		const outputs: InferenceSession.ReturnType = await this.session!!.run({
 			input_ids: inputIdsTensor,
 			attention_mask: attentionMaskTensor,
 			token_type_ids: tokenTypeIdsTensor
@@ -38,8 +44,7 @@ export class LocalInference {
 		let infer = new Tensor('float32', new Float32Array(result.cpuData), result.dims);
 		let output = mean_pooling(infer, encodings.attention_mask);
 
-		let final = reshape(output.data, output.dims as any);
-		return final;
+		return reshape(output.data, output.dims as any);
 	}
 }
 
--- src/test/codesearch/LocalInference.test.ts
+++ src/test/codesearch/LocalInference.test.ts
@@ -3,6 +3,7 @@ import { LocalInference } from "../../code-search/embedding/LocalInference";
 describe('LocalInference', () => {
 	it.skip('should encoding', async () => {
 		let localInference = new LocalInference();
+		await localInference.init();
 		let promise = await localInference.embed("test");
 		console.log(promise);
 	});
