b19cf125e62985d055846a7463ba8c6166f6a0b7
Phodal Huang <h@phodal.com>
2024-04-30 22:14:21 +0800

    feat(code-search): add SearchElement and SearchElementBuilder classes #19
    This commit introduces two new classes, `SearchElement` and `SearchElementBuilder`, in the `code-search` module. The `SearchElement` interface represents a search element with properties such as `uri`, `beforeCursor`, `afterCursor`, `languageId`, `path`, and `document`. The `SearchElementBuilder` class is used to generate a `SearchElement` from a given text editor. This enhancement is part of the Visual Studio Code (vscode) extension API.

---
+++ src/code-search/similar/SearchElement.ts
@@ -0,0 +1,53 @@
+import vscode, { TextDocument, Uri } from "vscode";
+
+export interface SearchElement {
+	uri: Uri,
+	beforeCursor: string,
+	afterCursor: string,
+	languageId: string,
+	path: string,
+	document: TextDocument
+}
+
+/**
+ * The `SearchElementGenerator` class is used to generate a search element from a given text editor.
+ * This class is part of the Visual Studio Code (vscode) extension API.
+ */
+export class SearchElementBuilder {
+	private editor: vscode.TextEditor;
+
+	constructor(editor: vscode.TextEditor) {
+		this.editor = editor;
+	}
+
+	build(): SearchElement {
+		const document = this.editor.document;
+		const cursorPosition = this.editor.selection.active;
+		const beforeCursor = this.getBeforeCursor(document, cursorPosition);
+		const afterCursor = this.getAfterCursor(document, cursorPosition);
+		const languageId = document.languageId;
+		const uri = document.uri;
+		const path = uri.path;
+
+		return {
+			uri,
+			beforeCursor,
+			afterCursor,
+			languageId,
+			path,
+			document
+		};
+	}
+
+	private getBeforeCursor(document: TextDocument, cursorPosition: vscode.Position): string {
+		const start = new vscode.Position(cursorPosition.line, 0);
+		const range = new vscode.Range(start, cursorPosition);
+		return document.getText(range);
+	}
+
+	private getAfterCursor(document: TextDocument, cursorPosition: vscode.Position): string {
+		const end = new vscode.Position(cursorPosition.line, document.lineAt(cursorPosition.line).range.end.character);
+		const range = new vscode.Range(cursorPosition, end);
+		return document.getText(range);
+	}
+}
