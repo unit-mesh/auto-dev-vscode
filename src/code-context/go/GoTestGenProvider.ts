0cd45dafacaf4a53ae51060f0fcb55bad7d22a84
Phodal Huang <h@phodal.com>
2024-05-03 14:08:32 +0800

    feat(test-generation): implement test file creation for Go language #24
    Implemented the `findOrCreateTestFile` method in `GoTestGenProvider` to support automatic test file creation for Go language. The method checks if a test file exists for a given source file, if not, it creates one. It also sets up the `AutoTestTemplateContext` for the test file.

--- src/code-context/go/GoTestGenProvider.ts
+++ src/code-context/go/GoTestGenProvider.ts
@@ -1,15 +1,18 @@
 import { injectable } from "inversify";
-import { TextDocument } from "vscode";
+import vscode, { TextDocument } from "vscode";
 
 import { NamedElement } from "../../editor/ast/NamedElement";
 import { CodeStructure } from "../../editor/codemodel/CodeFile";
 import { TSLanguageService } from "../../editor/language/service/TSLanguageService";
 import { AutoTestTemplateContext } from "../_base/test/AutoTestTemplateContext";
 import { TestGenProvider } from "../_base/test/TestGenProvider";
+import path from "path";
+import fs from "fs";
 
 @injectable()
 export class GoTestGenProvider implements TestGenProvider {
 	private languageService: TSLanguageService | undefined;
+	context: AutoTestTemplateContext | undefined;
 
 	isApplicable(lang: string): boolean {
 		return lang === "go";
@@ -21,11 +24,56 @@ export class GoTestGenProvider implements TestGenProvider {
 		return Promise.resolve();
 	}
 
-	findOrCreateTestFile(sourceFile: TextDocument, element: NamedElement): Promise<AutoTestTemplateContext> {
-		throw new Error("Method not implemented.");
+	async findOrCreateTestFile(sourceFile: TextDocument, element: NamedElement): Promise<AutoTestTemplateContext> {
+		const testFilePath: vscode.Uri | undefined = this.getTestFilePath(sourceFile);
+
+		if (!testFilePath) {
+			return Promise.reject(`Failed to find test file path for: ${sourceFile}`);
+		}
+
+		const elementName = element.identifierRange.text;
+
+		if (fs.existsSync(testFilePath.toString())) {
+			const context: AutoTestTemplateContext = {
+				filename: sourceFile.fileName,
+				currentClass: undefined,
+				language: "",
+				relatedClasses: [],
+				testClassName: "",
+				imports: [],
+				targetPath: testFilePath.fsPath
+			};
+
+			this.context = context;
+			return context;
+		}
+
+		await vscode.workspace.fs.writeFile(testFilePath, new Uint8Array());
+		const context: AutoTestTemplateContext = {
+			filename: sourceFile.fileName,
+			currentClass: undefined,
+			language: "",
+			relatedClasses: [],
+			testClassName: elementName,
+			imports: [],
+			targetPath: testFilePath.fsPath
+		};
+
+		this.context = context;
+		return context;
+	}
+
+	getTestFilePath(element: vscode.TextDocument): vscode.Uri | undefined {
+		const extension = path.extname(element.uri.fsPath);
+		const elementName = path.basename(element.uri.fsPath, extension);
+
+		const testFileName = `${elementName}_test${extension}`;
+		const testFilePath = path.join(path.dirname(element.uri.fsPath), testFileName);
+
+		return vscode.Uri.file(testFilePath);
 	}
 
 	lookupRelevantClass(element: any): Promise<CodeStructure> {
-		throw new Error("Method not implemented.");
+		return Promise.reject("Method not implemented.");
 	}
 }
