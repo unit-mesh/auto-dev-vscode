0962b3694855dc31696393327e13bab9d3e31a79
Phodal Huang <h@phodal.com>
2024-05-10 14:01:45 +0800

    feat(build): add pre-download-build script for binaries
    This commit introduces a new pre-download-build.js script. This script is responsible for creating directories for different platforms and architectures, building binaries with pkg, and downloading and unzipping prebuilt sqlite3 binary for the target.

---
+++ pre-download-build.js
@@ -0,0 +1,51 @@
+/**
+ * Copyright 2023 Continue
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+const fs = require("fs");
+const { execSync } = require("child_process");
+
+fs.mkdirSync("dist/node_modules", { recursive: true });
+
+const platforms = ["darwin", "linux", "win32"];
+const architectures = ["x64", "arm64"];
+let targets = platforms.flatMap((platform) =>
+  architectures.map((arch) => `${platform}-${arch}`),
+);
+
+console.log("[info] Building binaries with pkg...");
+for (const target of targets) {
+  const targetDir = `dist/node_modules/bin/${target}`;
+  fs.mkdirSync(targetDir, { recursive: true });
+  console.log(`[info] Building ${target}...`);
+  // execSync(
+  //   `npx pkg --no-bytecode --public-packages "*" --public pkgJson/${target} --out-path ${targetDir}`,
+  // );
+
+  // Download and unzip prebuilt sqlite3 binary for the target
+  const downloadUrl = `https://github.com/TryGhost/node-sqlite3/releases/download/v5.1.7/sqlite3-v5.1.7-napi-v6-${
+    target === "win32-arm64" ? "win32-ia32" : target
+  }.tar.gz`;
+  execSync(`curl -L -o ${targetDir}/build.tar.gz ${downloadUrl}`);
+  execSync(`cd ${targetDir} && tar -xvzf build.tar.gz`);
+  fs.copyFileSync(
+    `${targetDir}/build/Release/node_sqlite3.node`,
+    `${targetDir}/node_sqlite3.node`,
+  );
+  fs.unlinkSync(`${targetDir}/build.tar.gz`);
+  fs.rmSync(`${targetDir}/build`, {
+    recursive: true,
+    force: true,
+  });
+}
