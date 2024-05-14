b292fc8dfb9442462664be00e410e4c72fb591a8
Phodal Huang <h@phodal.com>
2024-05-14 21:34:02 +0800

    refactor(llm): simplify LlmProvider instance creation
    Removed unnecessary constructor and instance check for a cleaner instance creation process.

--- src/llm-provider/LlmProvider.ts
+++ src/llm-provider/LlmProvider.ts
@@ -3,24 +3,8 @@ import { LlmConfig } from "../settings/LlmConfig";
 import { OpenAICompletion } from "./OpenAICompletion";
 
 export class LlmProvider {
-	private static instance_: LlmProvider;
-	private llmConfig: LlmConfig;
-
-	private constructor(llmConfig: LlmConfig) {
-		this.llmConfig = llmConfig;
-	}
-
 	public static instance(): OpenAICompletion {
-		// TODO Support for multiple providers
 		const llmConfig = SettingService.instance().getCodeCompletionConfig();
-		if (!LlmProvider.instance_) {
-			LlmProvider.instance_ = new LlmProvider(llmConfig);
-		}
-
-		return LlmProvider.instance_.create();
-	}
-
-	private create() {
-		return new OpenAICompletion(this.llmConfig);
+		return new OpenAICompletion(llmConfig);
 	}
 }
