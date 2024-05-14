b21b85e4b4dfe2578dcebe2eecb294d1950a6a4c
Phodal Huang <h@phodal.com>
2024-05-14 09:03:38 +0800

    feat(code-context): refactor relatedClassesContext method
    Added a new asynchronous method `relatedClassesContext` in `RelevantCodeProviderManager.ts`. This method retrieves the context of related classes for a given named element in a file. It also includes a new helper method `renderFiles` to render the related files.

--- src/code-context/RelevantCodeProviderManager.ts
+++ src/code-context/RelevantCodeProviderManager.ts
@@ -37,13 +37,35 @@ export class RelevantCodeProviderManager {
 		return relatedProvider;
 	}
 
+	/**
+	 * `relatedClassesContext` is an asynchronous method that retrieves the context of related classes for a given named element in a file.
+	 *
+	 * @param languageId - The ID of the supported language. This is used to get the relevant code provider.
+	 * @param file - An instance of `TreeSitterFile` which represents the file where the named element is located.
+	 * @param namedElement - An instance of `NamedElement` which represents the element for which related classes are to be found.
+	 * @param languageService - An instance of `TSLanguageService` which is used to provide language-specific services. If not provided, a new instance of `DefaultLanguageService` is used.
+	 *
+	 * The method first gets the relevant code provider for the given language ID and language service. If a relevant provider is found, it retrieves the related files using the `getMethodFanInAndFanOut` method of the provider.
+	 *
+	 * The method then renders the related files and returns the result.
+	 *
+	 * @returns A promise that resolves with the rendered related files.
+	 *
+	 * @throws Will throw an error if the relevant code provider is not found.
+	 */
 	async relatedClassesContext(languageId: SupportedLanguage, file: TreeSitterFile, namedElement: NamedElement, languageService: TSLanguageService = new DefaultLanguageService()) {
 		let relatedProvider = RelevantCodeProviderManager.getInstance().provider(languageId, languageService);
-		let relatedFiles: CodeFile[] = [];
-		if (relatedProvider) {
-			relatedFiles = await relatedProvider.getMethodFanInAndFanOut(file, namedElement);
+		if (!relatedProvider) {
+			return Promise.resolve('');
 		}
 
+		let relatedFiles: CodeFile[] = [];
+		relatedFiles = await relatedProvider.getMethodFanInAndFanOut(file, namedElement);
+
+		return this.renderFiles(relatedFiles);
+	}
+
+	renderFiles(relatedFiles: CodeFile[]) {
 		let umlPresenter = new CommentedUmlPresenter();
 		return relatedFiles.map((codeStructure) => {
 			return umlPresenter.present(codeStructure);
