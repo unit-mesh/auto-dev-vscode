bac95ce4cf053a777a8cf9257021495b94195f13
Phodal Huang <h@phodal.com>
2024-05-16 16:00:33 +0800

    feat(llm-provider): add support for dynamic strategy
    Add support for dynamic strategy in the LlmProvider class. The strategy determines the model to be used for chat completion. The model is selected based on the provided strategy, with options for high-quality, high-speed, and long-context. This allows for more flexibility in choosing the appropriate model for different scenarios.

--- src/llm-provider/LlmProvider.ts
+++ src/llm-provider/LlmProvider.ts
@@ -4,7 +4,8 @@ import { OpenAICompletion } from "./OpenAICompletion";
 export enum LlmStrategy {
 	Normal = "normal",
 	HighQuality = "high-quality",
-	HighSpeed = "high-speed"
+	HighSpeed = "high-speed",
+	LongContext = "long-context",
 }
 
 export class LlmProvider {
@@ -15,6 +16,24 @@ export class LlmProvider {
 
 	public static chatCompletion(strategy: LlmStrategy = LlmStrategy.Normal): OpenAICompletion {
 		const llmConfig = SettingService.instance().getChatCompletionConfig();
+		// dynamic strategy
+
+		// based on https://platform.openai.com/docs/models/gpt-3-5-turbo
+		// there will be same model for high-quality and high-speed, and maybe gpt-4-turbo for 128k?
+		if (llmConfig.baseURL?.includes("https://api.openai.com/")) {
+			switch (strategy) {
+				case LlmStrategy.HighQuality:
+					llmConfig.model = "gpt-3.5-turbo";
+					break;
+				case LlmStrategy.HighSpeed:
+					llmConfig.model = "gpt-3.5-turbo";
+					break;
+				case LlmStrategy.LongContext:
+					llmConfig.model = "gpt-3.5-turbo";
+					break;
+			}
+		}
+
 		return new OpenAICompletion(llmConfig);
 	}
 }
