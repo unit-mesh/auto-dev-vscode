f1d2b277dee2ce6bc0b8c08067250c8c3cba1d82
Phodal Huang <h@phodal.com>
2024-05-19 12:01:30 +0800

    feat(embedding): add support for named elements in chunk items #30
    Add support for named elements in chunk items by introducing the NamedChunkItem interface.
    
    This commit adds the NamedChunkItem interface with properties for name, path, text, score, range, named element, and embedding.

--- src/code-search/embedding/_base/Embedding.ts
+++ src/code-search/embedding/_base/Embedding.ts
@@ -1,4 +1,5 @@
 import { TextRange } from "../../scope-graph/model/TextRange";
+import { NamedElement } from "../../../editor/ast/NamedElement";
 
 export type Embedding = number[];
 
@@ -14,4 +15,15 @@ export interface ChunkItem {
 	range: TextRange;
 	embedding: Embedding;
 	score?: number;
+}
+
+// todo: add parse for named element
+export interface NamedChunkItem {
+	name: string;
+	path: string;
+	text: string;
+	score: number;
+	range: TextRange;
+	nameElement: NamedElement;
+	embedding: Embedding;
 }
