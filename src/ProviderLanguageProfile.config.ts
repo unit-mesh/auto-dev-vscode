c35c536c2f704095ef4f9da241c0701dbae2e09a
Phodal Huang <h@phodal.com>
2024-05-23 11:06:48 +0800

    feat(PythonProfile): add Python language support #38
    This commit introduces Python language support by adding a new PythonProfile class and binding it to the language container. This includes defining language-specific queries, built-in types, and other language-specific configurations.

--- src/ProviderLanguageProfile.config.ts
+++ src/ProviderLanguageProfile.config.ts
@@ -4,11 +4,13 @@ import { PROVIDER_TYPES } from "./ProviderTypes";
 import { JavaProfile } from "./code-context/java/JavaProfile";
 import { TypeScriptProfile } from "./code-context/typescript/TypeScriptProfile";
 import { GolangProfile } from "./code-context/go/GolangProfile";
+import { PythonProfile } from "./code-context/python/PythonProfile";
 
 const languageContainer = new Container();
 
 languageContainer.bind<LanguageProfile>(PROVIDER_TYPES.LanguageProfile).to(JavaProfile);
 languageContainer.bind<LanguageProfile>(PROVIDER_TYPES.LanguageProfile).to(TypeScriptProfile);
 languageContainer.bind<LanguageProfile>(PROVIDER_TYPES.LanguageProfile).to(GolangProfile);
+languageContainer.bind<LanguageProfile>(PROVIDER_TYPES.LanguageProfile).to(PythonProfile);
 
 export { languageContainer };
