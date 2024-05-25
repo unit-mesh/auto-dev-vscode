f9278ed1fbda9cce21cd90ed2fb0c51635d86b6d
Phodal Huang <h@phodal.com>
2024-05-25 11:02:30 +0800

    feat(gui-sidebar): add type checking to model selector function

--- gui-sidebar/src/redux/selectors/modelSelectors.ts
+++ gui-sidebar/src/redux/selectors/modelSelectors.ts
@@ -1,9 +1,10 @@
 import { DEFAULT_MAX_TOKENS } from "../../shims/llm-constants";
 import { RootState } from "../store";
+import { ModelDescription } from "../../shims/typings";
 
 export const defaultModelSelector = (state: RootState) => {
   const title = state.state.defaultModelTitle ?? "";
-  return state.state.config.models.find((model) => model.title === title);
+  return state.state.config.models.find((model: ModelDescription) => model.title === title)
 };
 
 export const contextLengthSelector = (state: RootState) => {
