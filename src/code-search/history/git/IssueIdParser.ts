11e0eb88f4a489d91a38944e4d53b7a684a857b8
Phodal Huang <h@phodal.com>
2024-05-30 14:09:52 +0800

    feat(git): add IssueIdParser class to parse issue id from commit message #47
    This commit introduces a new class, IssueIdParser, in the git history module. This class provides methods to parse the issue id from a commit message and fetch commits by issue id or by a single file path.

---
+++ src/code-search/history/git/IssueIdParser.ts
@@ -0,0 +1,29 @@
+import { GitCommit } from "../_base/GitCommit";
+
+/**
+ * Will parse the issue id from the commit message
+ */
+export class IssueIdParser {
+	/**
+	 * Parse the issue id from the commit message
+	 * @param commitMessage The commit message to parse
+	 * @returns The issue id
+	 */
+	parseIssueId(commitMessage: string): string[] {
+		return [];
+	}
+
+	/**
+	 *
+	 */
+	fetchCommitByIssueId(issueId: string): GitCommit[] {
+		return [];
+	}
+
+	/**
+	 * Fetch the commit by the issue id
+	 */
+	fetchCommitBySingleFile(filePath: string): GitCommit[] {
+		return [];
+	}
+}
